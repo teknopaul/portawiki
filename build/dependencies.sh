5ce2d2ee4ff2724c1dd08ad0d0f292edfcba22ed
teknopaul <teknopaul@gmail.com>
2011-08-28 21:17:02 +0200

    script to pull deps from npm

---
+++ build/dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# You need the mighty npm installed for this to work.
+#
+
+cd `dirname $0`
+cd ../..
+npm install dom-js
+npm install filter-chain
+npm install sax
+npm install rewrite
+npm install formidable
+
